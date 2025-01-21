d36bb994e2f9a1948b7039b3ddfc197362b29c11
GentlyTech <andyyep@hotmail.com>
2025-01-21 17:58:55 -0500

    Use viewBinding in HomeFragment

--- app/src/main/java/com/yepdevelopment/spammedaddy/Fragments/HomeFragment.java
+++ app/src/main/java/com/yepdevelopment/spammedaddy/Fragments/HomeFragment.java
@@ -2,6 +2,7 @@ package com.yepdevelopment.spammedaddy.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,16 +10,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.yepdevelopment.spammedaddy.R;
+import com.yepdevelopment.spammedaddy.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+    FragmentHomeBinding fragmentHomeBinding;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return FragmentHomeBinding.inflate(inflater, container, false).getRoot();
     }
 }
