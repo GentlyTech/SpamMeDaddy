6518c5a918b8e36e6e730e5ab0c5a0d8f2ce1e8d
GentlyTech <andyyep@hotmail.com>
2025-01-21 18:58:14 -0500

    Add toolbar

--- app/src/main/java/com/yepdevelopment/spammedaddy/Activities/MainActivity.java
+++ app/src/main/java/com/yepdevelopment/spammedaddy/Activities/MainActivity.java
@@ -7,24 +7,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-import com.yepdevelopment.spammedaddy.R;
 import com.yepdevelopment.spammedaddy.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding mainActivityBinding;
+    ActivityMainBinding binding;
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mainActivityBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(mainActivityBinding.getRoot());
-        ViewCompat.setOnApplyWindowInsetsListener(mainActivityBinding.getRoot(), (v, insets) -> {
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        NavHostFragment navHostFragment = binding.mainActivityNavView.getFragment();
+        if (navHostFragment == null) return;
+        navController = navHostFragment.getNavController();
+
+        setSupportActionBar(binding.mainActivityToolBar);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupWithNavController(binding.mainActivityToolBar, navController, appBarConfiguration);
     }
 }
