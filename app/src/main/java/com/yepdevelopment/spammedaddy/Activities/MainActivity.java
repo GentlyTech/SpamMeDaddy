1944c4a47b6d6aac8747cf756af005e90bffecd5
GentlyTech <andyyep@hotmail.com>
2025-01-21 16:11:57 -0500

    Migrate MainActivity to use viewBinding

--- app/src/main/java/com/yepdevelopment/spammedaddy/Activities/MainActivity.java
+++ app/src/main/java/com/yepdevelopment/spammedaddy/Activities/MainActivity.java
@@ -9,15 +9,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.yepdevelopment.spammedaddy.R;
+import com.yepdevelopment.spammedaddy.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mainActivityBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mainActivityBinding = ActivityMainBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(mainActivityBinding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(mainActivityBinding.getRoot(), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
