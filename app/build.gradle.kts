fd2c385b0a7afca001a9b2a46e970a1feaf5c6e6
GentlyTech <andyyep@hotmail.com>
2025-01-21 16:11:27 -0500

    Enable buildConfig and viewBinding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,12 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
