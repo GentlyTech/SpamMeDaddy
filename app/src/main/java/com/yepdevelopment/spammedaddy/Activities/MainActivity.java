0a7dbf528dae81b164902d9fb907a5befcf5ae0b
GentlyTech <andyyep@hotmail.com>
2025-01-21 22:16:05 -0500

    Set proper toolbar name on activity launch

--- app/src/main/java/com/yepdevelopment/spammedaddy/Activities/MainActivity.java
+++ app/src/main/java/com/yepdevelopment/spammedaddy/Activities/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -34,6 +35,11 @@ public class MainActivity extends AppCompatActivity {
         NavHostFragment navHostFragment = binding.mainActivityNavView.getFragment();
         if (navHostFragment == null) return;
         navController = navHostFragment.getNavController();
+
+        NavDestination startPage = navController.getCurrentDestination();
+        if (startPage != null) {
+            binding.mainActivityToolBar.setTitle(startPage.getLabel());
+        }
 
         setSupportActionBar(binding.mainActivityToolBar);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
