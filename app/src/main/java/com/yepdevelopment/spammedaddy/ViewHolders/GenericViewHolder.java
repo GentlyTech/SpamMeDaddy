0bbc44cc318d2063f4c1e76fce5f6526d0c2d153
GentlyTech <andyyep@hotmail.com>
2025-01-21 20:57:48 -0500

    Add GenericViewHolder

---
+++ app/src/main/java/com/yepdevelopment/spammedaddy/ViewHolders/GenericViewHolder.java
@@ -0,0 +1,18 @@
+package com.yepdevelopment.spammedaddy.ViewHolders;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewbinding.ViewBinding;
+
+public class GenericViewHolder<T extends ViewBinding> extends RecyclerView.ViewHolder {
+    private final T binding;
+
+    public GenericViewHolder(@NonNull T binding) {
+        super(binding.getRoot());
+        this.binding = binding;
+    }
+
+    public T getBinding() {
+        return binding;
+    }
+}
