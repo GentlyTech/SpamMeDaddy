e30b6b63e4a65f1fbc4638fa884a1addd6afa253
GentlyTech <andyyep@hotmail.com>
2025-01-22 00:32:10 -0500

    Implement rudimentary recipients layout

--- app/src/main/java/com/yepdevelopment/spammedaddy/Pages/AddRecipientPage.java
+++ app/src/main/java/com/yepdevelopment/spammedaddy/Pages/AddRecipientPage.java
@@ -3,6 +3,7 @@ package com.yepdevelopment.spammedaddy.Pages;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,6 +14,11 @@ import com.yepdevelopment.spammedaddy.R;
 import com.yepdevelopment.spammedaddy.databinding.PageAddRecipientBinding;
 
 public class AddRecipientPage extends Fragment {
+    public enum ContactsListHideReason {
+        NO_CONTACTS,
+        CONTACTS_DISALLOWED
+    }
+
     PageAddRecipientBinding binding;
 
     @Override
@@ -25,5 +31,32 @@ public class AddRecipientPage extends Fragment {
                              Bundle savedInstanceState) {
         binding = PageAddRecipientBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        hideContactsList(ContactsListHideReason.NO_CONTACTS);
+    }
+
+    public void showContactsList() {
+        binding.addRecipientsContactsList.setVisibility(View.VISIBLE);
+        binding.addRecipientsContactsListStatusLayout.setVisibility(View.GONE);
+    }
+
+    public void hideContactsList(ContactsListHideReason reason) {
+        binding.addRecipientsContactsList.setVisibility(View.GONE);
+
+        switch (reason) {
+            case NO_CONTACTS:
+                binding.addRecipientsContactsListStatusHeading.setText(R.string.addRecipientPage_ContactsListStatusHeading_NoContacts);
+                binding.addRecipientsContactsListStatusBody.setText(R.string.addRecipientPage_ContactsListStatusBody_NoContacts);
+                binding.addRecipientsContactsListStatusLayout.setVisibility(View.VISIBLE);
+                break;
+            case CONTACTS_DISALLOWED:
+                binding.addRecipientsContactsListStatusHeading.setText(R.string.addRecipientPage_ContactsListStatusHeading_ContactsDisallowed);
+                binding.addRecipientsContactsListStatusBody.setText(R.string.addRecipientPage_ContactsListStatusBody_ContactsDisallowed);
+                binding.addRecipientsContactsListStatusLayout.setVisibility(View.VISIBLE);
+                break;
+        }
     }
 }
