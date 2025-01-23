e2d46a9ee6aafed30722c72366ae6305c35a897c
GentlyTech <andyyep@hotmail.com>
2025-01-22 19:22:45 -0500

    Add ViewModel for AddRecipientPage

--- app/src/main/java/com/yepdevelopment/spammedaddy/ViewModels/AddRecipientPageViewModel.java
+++ app/src/main/java/com/yepdevelopment/spammedaddy/ViewModels/AddRecipientPageViewModel.java
@@ -1,7 +1,16 @@
 package com.yepdevelopment.spammedaddy.ViewModels;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class AddRecipientPageViewModel extends ViewModel {
+    private final MutableLiveData<String> _RecipientFieldText = new MutableLiveData<>("");
 
+    public String getRecipientFieldText() {
+        return _RecipientFieldText.getValue();
+    }
+
+    public void setRecipientFieldText(String newValue) {
+        _RecipientFieldText.setValue(newValue);
+    }
 }
