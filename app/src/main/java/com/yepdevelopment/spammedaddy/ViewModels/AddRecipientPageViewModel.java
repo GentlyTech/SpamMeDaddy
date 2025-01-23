751af10bac36804a36868bf914dea966af4de9e3
GentlyTech <andyyep@hotmail.com>
2025-01-22 19:26:50 -0500

    Fix ViewModel getter for RecipientFieldText

--- app/src/main/java/com/yepdevelopment/spammedaddy/ViewModels/AddRecipientPageViewModel.java
+++ app/src/main/java/com/yepdevelopment/spammedaddy/ViewModels/AddRecipientPageViewModel.java
@@ -1,13 +1,14 @@
 package com.yepdevelopment.spammedaddy.ViewModels;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class AddRecipientPageViewModel extends ViewModel {
     private final MutableLiveData<String> _RecipientFieldText = new MutableLiveData<>("");
 
-    public String getRecipientFieldText() {
-        return _RecipientFieldText.getValue();
+    public LiveData<String> getRecipientFieldText() {
+        return _RecipientFieldText;
     }
 
     public void setRecipientFieldText(String newValue) {
