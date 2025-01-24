bf188e461f552a8bab9833c22aafef2070491479
GentlyTech <andyyep@hotmail.com>
2025-01-24 00:20:18 -0500

    Add placeholder database classes Add database singleton getter

---
+++ app/src/main/java/com/yepdevelopment/spammedaddy/Database/AppDatabase.java
@@ -0,0 +1,9 @@
+package com.yepdevelopment.spammedaddy.Database;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+}
---
+++ app/src/main/java/com/yepdevelopment/spammedaddy/Utils/Android/DatabaseUtils.java
@@ -0,0 +1,19 @@
+package com.yepdevelopment.spammedaddy.Utils.Android;
+
+import android.content.Context;
+
+import androidx.room.Room;
+
+import com.yepdevelopment.spammedaddy.Database.AppDatabase;
+
+public class DatabaseUtils {
+
+    private static AppDatabase appDatabaseInst = null;
+
+    public static AppDatabase getAppDatabase(Context context) {
+        if (appDatabaseInst == null) {
+            appDatabaseInst = Room.databaseBuilder(context, AppDatabase.class, "spammedaddy_db").build();
+        }
+        return appDatabaseInst;
+    }
+}
