5ae5f633974d43de80c20de935d7a2dde85204a6
GentlyTech <andyyep@hotmail.com>
2025-01-26 00:32:33 -0500

    Mark contact as Entity

--- app/src/main/java/com/yepdevelopment/spammedaddy/Database/Entities/Contact.java
+++ app/src/main/java/com/yepdevelopment/spammedaddy/Database/Entities/Contact.java
@@ -1,7 +1,9 @@
 package com.yepdevelopment.spammedaddy.Database.Entities;
 
+import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+@Entity
 public class Contact {
     @PrimaryKey
     public String contactId;
