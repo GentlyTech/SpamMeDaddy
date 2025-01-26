66dc0701b602fbba1e226f40c7e3ba44e9fc311e
GentlyTech <andyyep@hotmail.com>
2025-01-25 21:54:10 -0500

    Merge relationships

--- app/src/main/java/com/yepdevelopment/spammedaddy/Database/Relationships/ContactWithData.java
+++ app/src/main/java/com/yepdevelopment/spammedaddy/Database/Relationships/ContactWithData.java
@@ -4,13 +4,16 @@ import androidx.room.Embedded;
 import androidx.room.Relation;
 
 import com.yepdevelopment.spammedaddy.Database.Entities.Contact;
+import com.yepdevelopment.spammedaddy.Database.Entities.Message;
 import com.yepdevelopment.spammedaddy.Database.Entities.PhoneNumber;
 
 import java.util.List;
 
-public class ContactWithPhoneNumbers {
+public class ContactWithData {
     @Embedded
     public Contact contact;
     @Relation(parentColumn = "contactId", entityColumn = "contactId")
     public List<PhoneNumber> phoneNumbers;
+    @Relation(parentColumn = "contactId", entityColumn = "contactId")
+    public List<Message> messages;
 }
