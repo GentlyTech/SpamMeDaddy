51cad6f48aacf25b38618ad1cfa043ef59808f85
GentlyTech <andyyep@hotmail.com>
2025-01-26 02:07:28 -0500

    Add placeholder NumberSelectionPage

---
+++ app/src/main/java/com/yepdevelopment/spammedaddy/Pages/NumberSelectionPage.java
@@ -0,0 +1,8 @@
+package com.yepdevelopment.spammedaddy.Pages;
+
+import com.yepdevelopment.spammedaddy.Types.Page;
+import com.yepdevelopment.spammedaddy.databinding.PageNumberSelectionBinding;
+
+public class NumberSelectionPage extends Page<PageNumberSelectionBinding> {
+
+}
