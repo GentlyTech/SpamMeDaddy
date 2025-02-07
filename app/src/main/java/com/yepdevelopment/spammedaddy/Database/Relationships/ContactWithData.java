7e52f2a01bfc85568bb372cb39b7772a1624ac5b
GentlyTech <andyyep@hotmail.com>
2025-02-07 15:40:10 -0500

    Add some helper methods to ContactWithData

--- app/src/main/java/com/yepdevelopment/spammedaddy/Database/Relationships/ContactWithData.java
+++ app/src/main/java/com/yepdevelopment/spammedaddy/Database/Relationships/ContactWithData.java
@@ -7,7 +7,9 @@ import com.yepdevelopment.spammedaddy.Database.Entities.Contact;
 import com.yepdevelopment.spammedaddy.Database.Entities.Message;
 import com.yepdevelopment.spammedaddy.Database.Entities.PhoneNumber;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 import lombok.Getter;
 
@@ -21,4 +23,26 @@ public class ContactWithData {
 
     @Relation(parentColumn = "contactId", entityColumn = "contactId")
     private List<Message> messages;
+
+    public void addPhoneNumber(String phoneNumber) {
+        PhoneNumber phoneNumberObj = new PhoneNumber();
+        phoneNumberObj.setPhoneNumber(phoneNumber);
+        phoneNumberObj.setContactId(contact.getContactId());
+        phoneNumberObj.setPhoneNumberId(UUID.randomUUID().toString());
+
+        phoneNumbers.add(phoneNumberObj);
+    }
+
+    public static ContactWithData newInstance(String contactName) {
+        ContactWithData contactWithData = new ContactWithData();
+
+        contactWithData.contact = new Contact();
+        contactWithData.contact.setContactName(contactName);
+        contactWithData.contact.setContactId(UUID.randomUUID().toString());
+
+        contactWithData.phoneNumbers = new LinkedList<>();
+        contactWithData.messages = new LinkedList<>();
+
+        return contactWithData;
+    }
 }
