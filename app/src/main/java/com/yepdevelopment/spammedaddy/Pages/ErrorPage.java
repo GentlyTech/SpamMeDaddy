533d592f12b1f4f45a895c50722023ad794a4d87
GentlyTech <andyyep@hotmail.com>
2025-02-07 22:22:56 -0500

    Add generic error page (layout and nav)

---
+++ app/src/main/java/com/yepdevelopment/spammedaddy/Pages/ErrorPage.java
@@ -0,0 +1,8 @@
+package com.yepdevelopment.spammedaddy.Pages;
+
+import com.yepdevelopment.spammedaddy.Types.Page;
+import com.yepdevelopment.spammedaddy.databinding.PageErrorBinding;
+
+public class ErrorPage extends Page<PageErrorBinding> {
+
+}
