e4e7cf4116f1114b5080a08a5be494ca67430a56
GentlyTech <andyyep@hotmail.com>
2025-02-07 22:15:28 -0500

    Remove unused references/redundant override

--- app/src/main/java/com/yepdevelopment/spammedaddy/Pages/AboutPage.java
+++ app/src/main/java/com/yepdevelopment/spammedaddy/Pages/AboutPage.java
@@ -1,14 +1,10 @@
 package com.yepdevelopment.spammedaddy.Pages;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.yepdevelopment.spammedaddy.Adapters.ContributorAdapter;
@@ -21,10 +17,5 @@ public class AboutPage extends Page<PageAboutBinding> {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         binding.contributorsList.setLayoutManager(new LinearLayoutManager(requireContext()));
         binding.contributorsList.setAdapter(new ContributorAdapter(requireContext(), ResourceManipulator.getContributors(requireContext())));
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
     }
 }
