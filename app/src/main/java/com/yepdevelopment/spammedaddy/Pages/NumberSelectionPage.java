13a43f50b5eef1c43029fceddc7e99e7b8e68056
GentlyTech <andyyep@hotmail.com>
2025-02-11 17:12:44 -0500

    Add args to NumberSelectionPage

--- app/src/main/java/com/yepdevelopment/spammedaddy/Pages/NumberSelectionPage.java
+++ app/src/main/java/com/yepdevelopment/spammedaddy/Pages/NumberSelectionPage.java
@@ -6,6 +6,8 @@ import com.yepdevelopment.spammedaddy.R;
 import com.yepdevelopment.spammedaddy.Types.Page;
 import com.yepdevelopment.spammedaddy.databinding.PageNumberSelectionBinding;
 
+import lombok.Getter;
+
 public class NumberSelectionPage extends Page<PageNumberSelectionBinding> {
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,5 +20,17 @@ public class NumberSelectionPage extends Page<PageNumberSelectionBinding> {
             errorPageArgs.putString(ErrorPage.ARGS.BODY.getValue(), getString(R.string.numberSelectionPage_ErrorPageBody_NoContact));
             navController.navigate(R.id.action_numberSelectionPage_to_errorPage, errorPageArgs);
 //        }
+    }
+
+    @Getter
+    public enum ARGS {
+        CONTACT("contact");
+
+        private final String value;
+
+        ARGS(String value) {
+            this.value = value;
+        }
+
     }
 }
