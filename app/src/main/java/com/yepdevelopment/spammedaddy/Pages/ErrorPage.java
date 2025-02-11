817f7ea7de25750488e5d56c673d26d4c349e3c6
GentlyTech <andyyep@hotmail.com>
2025-02-11 17:08:32 -0500

    Remove unused methods

--- app/src/main/java/com/yepdevelopment/spammedaddy/Pages/ErrorPage.java
+++ app/src/main/java/com/yepdevelopment/spammedaddy/Pages/ErrorPage.java
@@ -15,46 +15,6 @@ public class ErrorPage extends Page<PageErrorBinding> {
     private String headingMessage = null;
     private String bodyMessage = null;
 
-    public static ErrorPage newInstance() {
-        return new ErrorPage();
-    }
-
-    public static ErrorPage newInstance(String headingMessage, String bodyMessage) {
-        Bundle args = new Bundle();
-
-        args.putString("headingMessage", headingMessage);
-        args.putString("bodyMessage", bodyMessage);
-
-        ErrorPage inst = new ErrorPage();
-        inst.setArguments(args);
-        return inst;
-    }
-
-    public static ErrorPage newInstance(int headingMessageResId, int bodyMessageResId) {
-        ErrorPage inst = new ErrorPage();
-        Resources resources = inst.getResources();
-        Bundle args = new Bundle();
-
-        try {
-            String headingMessage = resources.getString(headingMessageResId);
-            args.putString("headingMessage", headingMessage);
-        }
-        catch (Resources.NotFoundException ex) {
-            Log.w(ErrorPage.class.getName(), "newInstance(): Invalid headingMessageResId (resource does not exist)");
-        }
-
-        try {
-            String bodyMessage = resources.getString(bodyMessageResId);
-            args.putString("bodyMessage", bodyMessage);
-        }
-        catch (Resources.NotFoundException ex) {
-            Log.w(ErrorPage.class.getName(), "newInstance(): Invalid bodyMessageResId (resource does not exist)");
-        }
-
-        inst.setArguments(args);
-        return inst;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
