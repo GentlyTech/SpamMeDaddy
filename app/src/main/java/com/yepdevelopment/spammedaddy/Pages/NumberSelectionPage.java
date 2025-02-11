3b00ecd1a9a4547d6d319f7e7e9946826d2ed21b
GentlyTech <andyyep@hotmail.com>
2025-02-11 17:23:57 -0500

    Implement loading ContactWithData in NumberSelectionPage (serialization/deserialization still needs to be implemented) Add error handling for NumberSelectionPage init

--- app/src/main/java/com/yepdevelopment/spammedaddy/Pages/NumberSelectionPage.java
+++ app/src/main/java/com/yepdevelopment/spammedaddy/Pages/NumberSelectionPage.java
@@ -2,6 +2,7 @@ package com.yepdevelopment.spammedaddy.Pages;
 
 import android.os.Bundle;
 
+import com.yepdevelopment.spammedaddy.Database.Relationships.ContactWithData;
 import com.yepdevelopment.spammedaddy.R;
 import com.yepdevelopment.spammedaddy.Types.Page;
 import com.yepdevelopment.spammedaddy.databinding.PageNumberSelectionBinding;
@@ -9,17 +10,34 @@ import com.yepdevelopment.spammedaddy.databinding.PageNumberSelectionBinding;
 import lombok.Getter;
 
 public class NumberSelectionPage extends Page<PageNumberSelectionBinding> {
+    ContactWithData contact;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Bundle args = getArguments();
 
-//        if (args == null) { // TODO implement checking whether a valid ContactWithData was actually passed rather than always showing error page
-            Bundle errorPageArgs = new Bundle();
-            errorPageArgs.putString(ErrorPage.ARGS.BODY.getValue(), getString(R.string.numberSelectionPage_ErrorPageBody_NoContact));
-            navController.navigate(R.id.action_numberSelectionPage_to_errorPage, errorPageArgs);
-//        }
+        if (args == null) {
+            navigateToErrorPage();
+            return;
+        }
+
+        String rawContactWithData = args.getString(ARGS.CONTACT.getValue());
+        ContactWithData contact = ContactWithData.fromJson(rawContactWithData);
+        if (contact == null) {
+            navigateToErrorPage();
+            return;
+        }
+
+        this.contact = contact;
+    }
+
+    private void navigateToErrorPage() {
+        Bundle errorPageArgs = new Bundle();
+        errorPageArgs.putString(ErrorPage.ARGS.HEADING.getValue(), getString(R.string.numberSelectionPage_ErrorPageHeading_NoContact));
+        errorPageArgs.putString(ErrorPage.ARGS.BODY.getValue(), getString(R.string.numberSelectionPage_ErrorPageBody_NoContact));
+        navController.navigate(R.id.action_numberSelectionPage_to_errorPage, errorPageArgs);
     }
 
     @Getter
